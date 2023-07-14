3721fdeed045cb266d1a5a290f8bd9401b8205b2
RithikDhanush <115940712+RithikDhanush@users.noreply.github.com>
2023-07-14 11:09:01 +0530

    Update subscriber.js

--- src/models/subscriber.js
+++ src/models/subscriber.js
@@ -1,3 +1,6 @@
+// This code defines a Mongoose schema for a subscriber with three fields: name, subscribedChannel, and subscribedDate. 
+// The name and subscribedChannel fields are of type String and are required. 
+// The subscribedDate field is of type Date, required, and has a default value of the current date and time.
 const mongoose = require("mongoose");
 
 const subscriberSchema = new mongoose.Schema({
@@ -16,4 +19,6 @@ const subscriberSchema = new mongoose.Schema({
     },
 });
 
+// The code exports a Mongoose model named "Subscriber" based on the defined schema. 
+// This model can be used to perform CRUD operations on the subscribers collection in the connected MongoDB database.
 module.exports = mongoose.model("Subscriber", subscriberSchema);
