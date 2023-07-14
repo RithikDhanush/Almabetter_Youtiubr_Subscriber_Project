936f632ed05f5ba151bd60326097638803d93ea2
RithikDhanush <115940712+RithikDhanush@users.noreply.github.com>
2023-07-14 11:01:57 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,15 +1,18 @@
+// This code exports an array of objects named data. Each object represents a subscriber and contains name and subscribedChannel properties. 
+// The exported data array can be used in other modules or files to access and manipulate the subscriber information.
+
 const data = [
     {
-        name: "John Doe",
-        subscribedChannel: "freeCodeCamp.org",
+        name: "John Wick",
+        subscribedChannel: "CNET",
     },
     {
         name: "Jeread Krus",
-        subscriberdChannel: "CNET",
+        subscriberdChannel: "Sentex",
     },
     {
         name: "Lucifer",
-        subscribedChannel: "sentex",
+        subscribedChannel: "freeCodeCamp.org",
     },
 ];
 
